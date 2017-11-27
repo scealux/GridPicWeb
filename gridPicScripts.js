04f004fc5acab1c852f0614c99e3a1b8fb11cbf7
jusa1470 <jusa1470@colorado.edu>
2017-11-27 11:28:24 -0700

    Fixed to newest update

--- gridPicScripts.js
+++ gridPicScripts.js
@@ -1,30 +1,224 @@
 //This is the javascript file
 //This is a change
 
-navigator.mediaDevices.getUserMedia({video:true}).then(function(stream){
 
-  vid.onloadedmetadata = function(){
-    this.width = overlay.width = this.videoWidth;
-    this.height = overlay.height = this.videoHeight;
+navigator.mediaDevices.getUserMedia({video:true}).then(function(stream){
+    vid.onloadedmetadata = function(){
+        this.width = overlay.width = this.videoWidth;
+        this.height = overlay.height = this.videoHeight;
     }
-  vid.src = URL.createObjectURL(stream);
-  vid.play();
-  overlay.onclick = function(){
-    var c = document.createElement('canvas');
-    c.width = vid.videoWidth;
-    c.height = vid.videoHeight;
-    c.getContext('2d').drawImage(vid, 0,0);
-    c.toBlob(doWhatYouWantWithTheCapturedImage);
+    vid.src = URL.createObjectURL(stream);
+    vid.play();
+    overlay.onclick = function(){
+        var c = document.createElement('canvas');
+        c.width = vid.videoWidth;
+        c.height = vid.videoHeight;
+        c.getContext('2d').drawImage(vid, 0,0);
+        c.toBlob(doWhatYouWantWithTheCapturedImage);
     };
-  });
+});
 
 function doWhatYouWantWithTheCapturedImage(blob){
-  var url = URL.createObjectURL(blob);
-  var img = new Image();
-  img.onload = function(){URL.revokeObjectURL(url);};
-  img.src = url;
-  URL.revokeObjectURL(vid.src);
-  overlay.parentNode.appendChild(img);
-  vid.parentNode.removeChild(vid);
-  overlay.parentNode.removeChild(overlay);
-  }
+    var url = URL.createObjectURL(blob);
+    var img = new Image();
+    img.onload = function(){URL.revokeObjectURL(url);};
+    img.src = url;
+    URL.revokeObjectURL(vid.src);
+    overlay.parentNode.appendChild(img);
+    vid.parentNode.removeChild(vid);
+    overlay.parentNode.removeChild(overlay);
+    
+    handFileSelect(blob);
+}
+
+/**
+    * Handles the sign in button press.
+*/
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+    } else {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+            alert('Please enter an email address.');
+            return;
+        }
+        if (password.length < 4) {
+            alert('Please enter a password.');
+            return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+            document.getElementById('quickstart-sign-in').disabled = false;
+            // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+    }
+    document.getElementById('quickstart-sign-in').disabled = true;
+}
+
+/**
+* Handles the sign up button press.
+*/
+function handleSignUp() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+    }
+    if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+    // [END createwithemail]
+}
+
+function sendPasswordReset() {
+    var email = document.getElementById('email').value;
+    // [START sendpasswordemail]
+    firebase.auth().sendPasswordResetEmail(email).then(function() {
+        // Password Reset Email Sent!
+        // [START_EXCLUDE]
+        alert('Password Reset Email Sent!');
+        // [END_EXCLUDE]
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/invalid-email') {
+            alert(errorMessage);
+        } else if (errorCode == 'auth/user-not-found') {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+    // [END sendpasswordemail];
+}
+
+/**
+* initApp handles setting up UI event listeners and registering Firebase auth listeners:
+*  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+*    out, and that is where we update the UI.
+*/
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function(user) {
+        // [START_EXCLUDE silent]
+        document.getElementById('quickstart-verify-email').disabled = true;
+        // [END_EXCLUDE]
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            
+            // document.getElementById('file').addEventListener('change', handleFileSelect, false);
+            
+            if (!emailVerified) {
+                document.getElementById('quickstart-verify-email').disabled = false;
+            }
+            // [END_EXCLUDE]
+        } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            
+            //document.getElementById('file').disabled = false;
+            // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE silent]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+    }) ;
+    // [END authstatelistener]
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+    document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
+    document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+    document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+}
+
+function openTab(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function handleFileSelect(blob){
+    var firebaseStorage = firebase.storage().ref();
+    console.log("test");
+    //evt.stopPropagation();
+    //evt.preventDefault();
+    var file = blob;
+    var metadata = {'contentType': file.type};
+
+    firebaseStorage.child('images/' + file.name).put(file, metadata).then(function(snapshot){
+        console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+        console.log(snapshot.metadata);
+        var url = snapshot.downloadURL;
+        console.log('File available at', url);
+    }).catch(function(error){
+        console.error('Upload failed:', error);  
+    });
+}
+
+window.onload = function() {
+    initApp();
+};
